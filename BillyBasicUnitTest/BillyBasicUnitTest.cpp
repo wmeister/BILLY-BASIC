7486d8903f613bd9882e51429ba4312a051c1aa2
William Meister <wmeister86@gmail.com>
2021-02-06 23:39:34 -0500

    added unit test project

---
+++ BillyBasicUnitTest/BillyBasicUnitTest.cpp
@@ -0,0 +1,34 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+#include <string>
+#include <vector>
+#include "../BillyBasic/BillyBasic.h"
+#include "../BillyBasic/common/string.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+using std::string;
+using std::vector;
+
+namespace BillyBasicUnitTest
+{
+	TEST_CLASS(BillyBasicUnitTest)
+	{
+	public:
+		
+		TEST_METHOD(TestArguments)
+		{
+			vector<string> args = arguments(" PRINT1   ");
+			Assert::AreEqual(1, (int)args.size());
+			Assert::AreEqual("1", args[0].c_str());
+
+			args = arguments("PRINT 1");
+			Assert::AreEqual(1, (int)args.size());
+			Assert::AreEqual("1", args[0].c_str());
+		}
+		
+		TEST_METHOD(TestStringUpcase)
+		{
+			Assert::AreEqual("PRINT", common::string::upcase("print").c_str());
+		}
+	};
+}
