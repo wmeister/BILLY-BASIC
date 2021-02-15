894dfb510802916823630372949c57ec21754599
William Meister <wmeister86@gmail.com>
2021-02-15 13:55:55 -0500

    token typedef struct; forgot semicolon

--- BillyBasic/BillyBasic.h
+++ BillyBasic/BillyBasic.h
@@ -3,10 +3,12 @@
 #include <string>
 #include <vector>
 
+typedef struct { std::string type; std::string raw; } token;
 void print(std::vector<std::string>);
 std::vector<std::string> arguments(std::string);
 void eval(char*);
 void read();
 int main();
-void syntax_error()
+void syntax_error();
 void BillyBasic();
+std::vector<token> tokenize(vector<string>);
